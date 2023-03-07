faa0cbbc3762a43a6d29949261754681fb2a6bb4
yobafromstarvvars <skavronsky.vt@gmail.com>
2023-03-07 22:46:36 +0300

    admin side nav moved to a separate file, navigation by post instead of links

--- app/admin/_admin-main.php
+++ app/admin/_admin-main.php
@@ -1,48 +1,3 @@
-<style>
-#main{
-    margin:0;
-    padding:0;
-}
-
-body {
-  font-family: "Lato", sans-serif;
-}
-
-.sidenav {
-  height: 100%;
-  width: 160px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
-
-.sidenav a {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 20px;
-  color: #818181;
-  display: block;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.main {
-  margin-left: 160px; /* Same as the width of the sidenav */
-  font-size: 28px; /* Increased text to enable scrolling */
-  padding: 0px 10px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-</style>
 
 
 <?php
@@ -75,19 +30,7 @@ body {
     }
 ?>
 
-<!-- Side nav -->
-<div class="sidenav">
-    <a href="<?=$gotoAdmin?>">Admin panel</a>
-    <a href="<?=$gotoHome  ?>">Main page</a>
-    <hr>
-    <a href="<?=$gotoAdminOrders  ?>">Orders</a>
-    <a href="<?=$gotoAdminUser ?>">Users</a>
-    <a href="<?=$gotoAdminProduct?>">Products</a>
-    <a href="<?=$gotoAdminCategory ?>">Categories</a>
-    <a href="<?=$gotoAdminSubcategory ?>">Subcategories</a>
-    <a href="<?=$gotoAdminType ?>">Types</a>
-    <a href="<?=$gotoAdminBrand ?>">Brands</a>
-</div>
+
 
 <!-- Main content -->
 <div class="main">
--- app/admin/admin.php
+++ app/admin/admin.php
@@ -4,8 +4,9 @@
     <body>
         <?php
             echo '<div id="main">';
+                require_once "_admin-sidenav.php";
                 require CHECK_ADMIN;
-require ADMIN_MAIN;      
+                require ADMIN_MAIN;      
             echo '</div>';
 
             // load all js scripts. Order is set in links.php
